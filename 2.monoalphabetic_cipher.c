d9fe7ab07fac4ccbac1eb48d2aeeb198fd21111a
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-05-31 08:58:55 +0530

    Update 2.monoalphabetic_cipher.c

--- 2.monoalphabetic_cipher.c
+++ 2.monoalphabetic_cipher.c
@@ -3,10 +3,10 @@
 void encrypt(char text[], char key[]) {
     int i;
     for (i = 0; text[i] != '\0'; i++) {
-        if (text[i] >= 'A' && text[i] <= 'Z') {
-            printf("%c", key[text[i] - 'A']);
-        } else if (text[i] >= 'a' && text[i] <= 'z') {
+        if (text[i] >= 'a' && text[i] <= 'z') {
             printf("%c", key[text[i] - 'a']);
+        } else if (text[i] >= 'A' && text[i] <= 'Z') {
+            printf("%c", key[text[i] - 'A']);
         } else {
             printf("%c", text[i]);
         }
@@ -17,17 +17,17 @@ void encrypt(char text[], char key[]) {
 void decrypt(char ciphertext[], char key[]) {
     int i, j;
     for (i = 0; ciphertext[i] != '\0'; i++) {
-        if (ciphertext[i] >= 'A' && ciphertext[i] <= 'Z') {
-            for (j = 0; key[j] != '\0'; j++) {
-                if (ciphertext[i] == key[j]) {
-                    printf("%c", 'A' + j);
-                    break;
-                }
-            }
-        } else if (ciphertext[i] >= 'a' && ciphertext[i] <= 'z') {
+        if (ciphertext[i] >= 'a' && ciphertext[i] <= 'z') {
             for (j = 0; key[j] != '\0'; j++) {
                 if (ciphertext[i] == key[j] + 'a' - 'A') {
                     printf("%c", 'a' + j);
+                    break;
+                }
+            }
+        } else if (ciphertext[i] >= 'A' && ciphertext[i] <= 'Z') {
+            for (j = 0; key[j] != '\0'; j++) {
+                if (ciphertext[i] == key[j]) {
+                    printf("%c", 'A' + j);
                     break;
                 }
             }
