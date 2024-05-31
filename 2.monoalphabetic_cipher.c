01c77e2cf3825bcfa506bb5b399b9424cfaa19f4
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-05-31 08:49:30 +0530

    Update 2.monoalphabetic_cipher.c

--- 2.monoalphabetic_cipher.c
+++ 2.monoalphabetic_cipher.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-void encrypt(char plaintext[], char key[]) {
+void encrypt(char text[], char key[]) {
     int i;
-    for (i = 0; plaintext[i] != '\0'; i++) {
-        if (plaintext[i] >= 'A' && plaintext[i] <= 'Z') {
-            printf("%c", key[plaintext[i] - 'A']);
-        } else if (plaintext[i] >= 'a' && plaintext[i] <= 'z') {
-            printf("%c", key[plaintext[i] - 'a']);
+    for (i = 0; text[i] != '\0'; i++) {
+        if (text[i] >= 'A' && text[i] <= 'Z') {
+            printf("%c", key[text[i] - 'A']);
+        } else if (text[i] >= 'a' && text[i] <= 'z') {
+            printf("%c", key[text[i] - 'a']);
         } else {
-            printf("%c", plaintext[i]);
+            printf("%c", text[i]);
         }
     }
     printf("\n");
@@ -39,12 +39,12 @@ void decrypt(char ciphertext[], char key[]) {
 }
 
 int main() {
-    char plaintext[] = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+    char text[] = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
     char key[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
 
-    printf("Plaintext: %s\n", plaintext);
+    printf("Text: %s\n", text);
     printf("Encrypted: ");
-    encrypt(plaintext, key);
+    encrypt(text, key);
 
     char ciphertext[] = "ZIT JXOEA WKGVF YGB PXDHL GCTK ZIT SQMN RGU";
     printf("Ciphertext: %s\n", ciphertext);
