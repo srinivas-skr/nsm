7d0108a8626c7b4179964d5fb9bdb8e3e7a3e67a
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-08-16 13:10:37 +0530

    Update 2.monoalphabetic_cipher.c

--- 2.monoalphabetic_cipher.c
+++ 2.monoalphabetic_cipher.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
 
 char encrypt(char ch, char *key) 
 {
@@ -27,38 +27,43 @@ char decrypt(char ch, char *key)
 
 int main() 
 {
-    char text[100];
+    char plaintext[100];
     char key[30];
-
-    // Input for text
-    printf("Enter text: ");
-    fgets(text, sizeof(text), stdin);
+    // Input for plaintext
+    printf("Enter plaintext: ");
+    fgets(plaintext, sizeof(plaintext), stdin);
 
     // Input for key
     printf("Enter key (26 uppercase letters): ");
     fgets(key, sizeof(key), stdin);
 
     // Encryption
-    char ciphertext[strlen(text) + 1];
+    char ciphertext[strlen(plaintext) + 1];
     int i;
-    for (i = 0; text[i] != '\0'; i++) 
+    for (i = 0; plaintext[i] != '\0'; i++) 
     {
-        ciphertext[i] = encrypt(text[i], key);
+        ciphertext[i] = encrypt(plaintext[i], key);
     }
     ciphertext[i] = '\0'; // Null terminate the ciphertext string
     printf("Ciphertext: %s\n", ciphertext);
-
-    // Input for ciphertext to decrypt
-    printf("Enter ciphertext to decrypt: ");
-    fgets(ciphertext, sizeof(ciphertext), stdin);
 
     // Decryption
     char decrypted_text[strlen(ciphertext) + 1];
     for (i = 0; ciphertext[i] != '\0'; i++) 
     {
-        decrypted_text[i] = decrypt(ciphertext[i], key);
+    decrypted_text[i] = decrypt(ciphertext[i], key);
     }
     decrypted_text[i] = '\0'; // Null terminate the decrypted text string
     printf("Decrypted Text: %s\n", decrypted_text);
     return 0;
 }
+
+/*plaintext - THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG
+key -QWERTYUIOPASDFGHJKLZXCVBNM */
+
+
+
+
+/* Input for ciphertext to decrypt
+    printf("Enter ciphertext to decrypt: ");
+    fgets(ciphertext, sizeof(ciphertext), stdin); */
