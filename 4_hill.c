da64d7bbbbf5e06c991d8716092ccfd32d991a90
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-08-18 16:43:40 +0530

    Update 4_hill.c

--- 4_hill.c
+++ 4_hill.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 
-void encrypt(char message[], int keyMatrix[10][10], int size)
+void encrypt(char msg[], int keyMatrix[10][10], int size)
 {
-    int messageLength = strlen(message);
+    int msgLen = strlen(msg);
     int i, j, k;
 
-    for (k = 0; k < messageLength; k += size) {
-        int paddedMessage[10] = {0};  // Fixed-size array for the message block
-        int encryptedMessage[10] = {0};  // Fixed-size array for the encrypted message
+    for (k = 0; k < msgLen; k += size) {
+        int paddedMsg[10] = {0};  // Fixed-size array for the message block
+        int encryptedMsg[10] = {0};  // Fixed-size array for the encrypted message
 
         // Pad with 'X' if the last block is shorter than 'size'
         for (i = 0; i < size; i++) {
-            paddedMessage[i] = (k + i < messageLength) ? message[k + i] - 'A' : 'X' - 'A';
+            paddedMsg[i] = (k + i < msgLen) ? msg[k + i] - 'A' : 'X' - 'A';
         }
 
         // Encryption: Multiply the key matrix with the message vector
         for (i = 0; i < size; i++) {
             for (j = 0; j < size; j++) {
-                encryptedMessage[i] += keyMatrix[i][j] * paddedMessage[j];
+                encryptedMsg[i] += keyMatrix[i][j] * paddedMsg[j];
             }
-            encryptedMessage[i] = encryptedMessage[i] % 26;  // Modulo 26 to stay within 'A' to 'Z'
+            encryptedMsg[i] = encryptedMsg[i] % 26;  // Modulo 26 to stay within 'A' to 'Z'
         }
-
+        
         // Convert back to characters and print the encrypted message
         for (i = 0; i < size; i++) {
-            printf("%c", encryptedMessage[i] + 'A');
+            printf("%c", encryptedMsg[i] + 'A');
         }
     }
     printf("\n");
@@ -44,10 +44,10 @@ int main() {
         }
     }
     
-    char message[100];  // Assuming the message won't exceed 100 characters
+    char msg[100];  // Assuming the message won't exceed 100 characters
     printf("Enter the message to be encrypted (uppercase letters only): ");
-    scanf("%s", message);
-    encrypt(message, keyMatrix, size);
+    scanf("%s", msg);
+    encrypt(msg, keyMatrix, size);
     return 0;
 }
 /* OUTPUT
